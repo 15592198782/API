ca284ccaa31f71d0ae3080a996131d5f7cdc59a2
15592198782 <773883967@qq.com>
2020-10-10 21:39:01 +0800

    modified

--- api/testcase/Test_001.py
+++ api/testcase/Test_001.py
@@ -1,7 +1,36 @@
 # import sys
 # sys.path.append("E:\github\zhaochaoyi\api")
 from database.RestClient import RestClient
+from github import Github
 r=RestClient("http://httpbin.org")
 x=r.post("/post",json={"a":"b"})
 print(x.text)
 
+
+r = Github(username="15592198782",password="zcy773883967")
+x=r.repos.list_your_repos()
+print(x.text)
+r = Github(token="xxxxxxxxxxxxxx")
+x=r.repos.list_your_repos()
+print(x.text)
+r = Github(token="xxxxxxxxxxxxxx")
+x=r.repos.list_your_repos(visibility="private")
+print(x.text)
+
+r = Github(token="xxxxxxxxxxxxxx")
+x=r.repos.list_your_repos(visibility="all")
+print(x.text)
+
+r = Github(token="xxxxxxxxxxxxxx")
+x=r.repos.list_your_repos(direction="desc")
+print(x.text)
+
+r = Github(token="xxxxxxxxxxxxxx")
+data={"direction":"desc"}
+x=r.repos.list_your_repos(params=data)
+print(x.text)
+
+r = Github(token="xxxxxxxxxxxxxx")
+data={"direction":"desc"}
+x=r.repos.list_user_repos("15592198782",params=data)
+print(x.text)
